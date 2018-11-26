8ce7ad4be4b90753ad88490f5462f85f287b9d43
Roy Bao <roy@roybao.com>
2018-11-26 15:04:06 -0800

    Refactor: use constants for displaying roles

--- src/components/user-fieldset.vue
+++ src/components/user-fieldset.vue
@@ -1,15 +1,7 @@
 <template>
 <div class="component-wrapper user-fieldset">
-  <el-form-item :error="errorMsgs.email" label="Email">
-    <el-input v-model="formModel.email" @input="updateValue()" name="email" placeholder="Enter a email..."></el-input>
-  </el-form-item>
-
   <el-form-item :error="errorMsgs.username" label="Username" required>
     <el-input v-model="formModel.username" @input="updateValue()" name="username" placeholder="Enter a username..."></el-input>
-  </el-form-item>
-
-  <el-form-item :error="errorMsgs.name" label="Name">
-    <el-input v-model="formModel.name" @input="updateValue()" name="name" placeholder="Enter a name..."></el-input>
   </el-form-item>
 
   <el-form-item v-if="!formModel.id" :error="errorMsgs.password" label="Password" required>
@@ -18,25 +10,33 @@
 
   <el-form-item :error="errorMsgs.role" label="Role" id="role" required>
     <el-select v-model="formModel.role" @change="updateValue()" placeholder="Select a role">
-      <el-option label="Read Only" value="readOnly"></el-option>
-      <el-option label="Support Specialist" value="supportSpecialist"></el-option>
-      <el-option label="Goods Specialist" value="goodsSpecialist"></el-option>
-      <el-option label="Marketing Specialist" value="marketingSpecialist"></el-option>
-      <el-option label="Manager" value="manager"></el-option>
-      <el-option label="Developer" value="developer"></el-option>
-      <el-option label="Administrator" value="administrator"></el-option>
+      <el-option v-for="role in roles" :key="role" :label="$t(`fields.user.role.${role}`)" :value="role"></el-option>
     </el-select>
+  </el-form-item>
+
+  <el-form-item :error="errorMsgs.email" label="Email">
+    <el-input v-model="formModel.email" @input="updateValue()" name="email" placeholder="Enter a email..."></el-input>
+  </el-form-item>
+
+  <el-form-item :error="errorMsgs.name" label="Name">
+    <el-input v-model="formModel.name" @input="updateValue()" name="name" placeholder="Enter a name..."></el-input>
   </el-form-item>
 </div>
 </template>
 
 <script>
+import { ROLES } from '@/env'
 import fieldsetMixinFactory from '@/mixins/fieldset'
 let FieldsetMixin = fieldsetMixinFactory({ errorI18nKey: 'user' })
 
 export default {
   name: 'UserFieldset',
-  mixins: [FieldsetMixin]
+  mixins: [FieldsetMixin],
+  data () {
+    return {
+      roles: ROLES
+    }
+  }
 }
 </script>
 
